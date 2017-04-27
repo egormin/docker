47ed2de8791a5c668845c823fdc1c3c5ee002a73
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-27 22:13:40 +0300

    fixed

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ docker build -t tomcat -f tomcat/tomcat.Dockerfile .
 docker build -t application -f application/application.Dockerfile .
 docker run -d --name app application
 docker run -d --name tomcat --volumes-from app tomcat
-docker run -d --name web -p 8080:80  --link tomcat:tomcat web
+docker run -d --name proxy -p 8080:80  --link tomcat:tomcat web
 curl -ILvk localhost:8080
 curl -ILvk localhost:8080/sample/
--- web/web.Dockerfile
+++ web/web.Dockerfile
@@ -1,5 +1,5 @@
 FROM sbeliakou/centos:7.2
 RUN yum install -y nginx
-COPY nginx.conf /etc/nginx/nginx.conf
+COPY web/nginx.conf /etc/nginx/nginx.conf
 EXPOSE 80
 CMD nginx -g 'daemon off;'
